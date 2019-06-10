53777a8cd20e54a08ad3b6b74e549e9a003438e2
muturi254 <petermuturi74@gmail.com>
2019-06-10 15:46:05 +0300

    corrected error in the urls.py

--- news/urls.py
+++ news/urls.py
@@ -5,6 +5,6 @@ from . import views
 # url routes 
 urlpatterns = [
     url(r'^$', views.welcome, name='welcome'),
-    url(r'^today/$', views.news_of_day , name='newsToady'),
+    url(r'^today/$', views.news_of_day , name='newsToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})$', views.past_days_news , name='pastNews'),
 ]
