ff50aa31b2a1e98f621e80f4ae0edd70c69760a7
muturi254 <petermuturi74@gmail.com>
2019-06-10 18:53:45 +0300

    add meta class to class editor

--- news/models.py
+++ news/models.py
@@ -5,3 +5,9 @@ class Editor(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
+
+    class Meta:
+        ordering = ['first_name']
+    # cange representation of data to string readable
+    def __str__(self):
+        return self.first_name
