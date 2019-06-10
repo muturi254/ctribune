a35c8fcf07632602c436af4e2edc5ef155b76515
muturi254 <petermuturi74@gmail.com>
2019-06-11 00:05:12 +0300

    context variables

--- news/views.py
+++ news/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
 
+from .models import Article
+
 # ustom method
 def convert_date(dates):
     # get weekday number
@@ -14,16 +16,19 @@ def convert_date(dates):
 # Create your views here.
 def news_of_day(request):
     date = dt.date.today()
-    return render(request, 'all-news/todays-news.html',{"date": date})
+    news = Article.todays_news()
+    return render(request, 'all-news/todays-news.html',{"date": date, "news":news})
 
 def past_days_news(request, past_date):
     try:
         # convert date from string input
         date =dt.datetime.strptime(past_date, '%Y-%m-%d').date()
     except ValueError:
+        assert False
         raise Http404()
     
     if date == dt.date.today():
         return redirect(news_of_day)
-    
-    return render(request, 'all-news/past-news.html', {"date": date})
+
+    news = Article.days_news(date)
+    return render(request, 'all-news/past-news.html', {"date": date, "news":news})
