7656642aa0c4d6c5f37264eb4271cdd0f921adae
muturi254 <petermuturi74@gmail.com>
2019-06-10 14:48:10 +0300

    added previous news and current days news

--- news/urls.py
+++ news/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 # url routes 
 urlpatterns = [
-    url(r'^$', views.welcome, name='welcome')
+    url(r'^$', views.welcome, name='welcome'),
+    url(r'^today/$', views.news_of_day , name='newsToady'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})$', views.past_days_news , name='pastNews'),
 ]
