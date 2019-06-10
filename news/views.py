f82b86b0eb87339d1110d723b57a2628b27d8865
muturi254 <petermuturi74@gmail.com>
2019-06-10 15:19:12 +0300

    updated all view with render shortcut

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
 
@@ -17,17 +17,7 @@ def welcome(request):
 
 def news_of_day(request):
     date = dt.date.today()
-
-    # convert date to day
-    day = convert_date(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day} - {date.month} -{date.year}</h1>
-            </body>
-        </html>
-    '''
-    return HttpResponse(html)
+    return render(request, 'all-news/todays-news.html',{"date": date})
 
 def past_days_news(request, past_date):
     try:
@@ -35,13 +25,8 @@ def past_days_news(request, past_date):
         date =dt.datetime.strptime(past_date, '%Y-%m-%d').date()
     except ValueError:
         raise Http404()
-    day = convert_date(date)
-
-    html =html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-    '''
-    return HttpResponse(html)
+    
+    if date == dt.date.today():
+        return redirect(news_of_day)
+    
+    return render(request, 'all-news/past-news.html', {"date": date})
