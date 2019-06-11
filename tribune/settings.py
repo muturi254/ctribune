3919db1c2d59b93e19bb9634431371a4e163bfae
muturi254 <petermuturi74@gmail.com>
2019-06-11 12:44:54 +0300

    changes

--- tribune/settings.py
+++ tribune/settings.py
@@ -33,8 +33,8 @@ if config("MODE") == "dev":
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': config('DB_NAME'),
             'USER': config('DB_USER'),
-            'PASSWORD': config('DB_PASSWORD')
-            'HOST': config('DB_HOST')
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
             'PORT': '',
 
         }
