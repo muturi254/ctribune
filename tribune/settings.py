451b3ec7a8ca287afad3d431ce46592589c79673
muturi254 <petermuturi74@gmail.com>
2019-06-11 13:54:24 +0300

    changes

--- tribune/settings.py
+++ tribune/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os, django_heroku, dj_database_url
-from python-decouple import config,Csv
+from py
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
